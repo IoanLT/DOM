e6e8a73e4c055d0af02a6001f686dc03c8b025c8
IoanLT <ioan.tranole@gmail.com>
2020-10-25 12:14:29 +0000

    Testing querySelector and querySelectorAll methods

---
+++ script.js
@@ -0,0 +1,22 @@
+// Grabing the first paragraph
+// const para = document.querySelector('p');
+// console.log(para);
+
+// Grabbing the element with the class .error
+// const para = document.querySelector('.error');
+// console.log(para);
+
+// Grabbing the div with the class .error
+// const para = document.querySelector('body > div:nth-child(2) > p:nth-child(1)');
+// console.log(para);
+
+// Grabbing all the p elements
+const paras = document.querySelectorAll('p');
+const errors = document.querySelectorAll('.error');
+
+
+paras.forEach(para => {
+    console.log(para);
+})
+console.log(errors);
+
